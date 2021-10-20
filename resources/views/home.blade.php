4eb49031a18e8af48d0d5d7c276857d7a78db10f
vivek <vkr@1118barrawnzgroup.com>
2021-10-20 14:39:58 +0530

    add all files

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+<x-layout>
+<x-slot name="title">
+Home
+</x-slot>
+<x-slot name="content">
+Main content
+</x-slot>
+</x-layout>
---
+++ resources/views/include/sidebar.blade.php
@@ -0,0 +1,5 @@
+<div class="border-end barder border-info h-100">
+ <div class="text-center">
+ <img src="{{asset('images/avtar.jpg')}}"alt=""class="img-fluid rounded rounded-circle mt-5"/>
+ </div>
+</div>
